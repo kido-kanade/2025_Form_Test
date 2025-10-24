8b183a051510ab96f177deaf9b8068fb489910a9
AzureAD\木戸奏(zeal25360163) <zoumuhu@gmail.com>
2025-10-24 15:38:20 +0900

    testbuttonコミット

--- Form_Test/Form_Test/Form1.Designer.cs
+++ Form_Test/Form_Test/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 91);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ボタン";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ラベル";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Form_Test
 {
@@ -15,6 +16,39 @@ namespace Form_Test
         public Form1()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    //インスタンス生成
+                    testbutton Testbutton = new testbutton();
+
+                    //ボタンの位置設定
+                    Testbutton.Location = new Point(50 * i, 50*j);
+
+
+                    //ボタンの大きさを設定
+                    Testbutton.Size = new Size(200, 100);
+
+                    //ボタン内のテキスト設定
+                    Testbutton.Text = "あいうえお";
+
+                    //コントロールボタンを追加
+                    Controls.Add(Testbutton);
+                }
+            }
+          
+            {
+
+            }
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("C#の世界へようこそ！");
         }
     }
 }

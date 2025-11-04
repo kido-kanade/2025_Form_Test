4ca372ee24ed1ddb30a21cf784ab9cf1b172a37c
AzureAD\木戸奏(zeal25360163) <zoumuhu@gmail.com>
2025-11-04 12:21:33 +0900

    ライツアウト課題前まで完成

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,28 +15,33 @@ namespace Form_Test
     public partial class Form1 : Form
     {
         //constをつけると初期化時のみ変更が可能になる
-        const int BUTTON_SIZE_X = 100;
-        const int BUTTON_SIZE_Y = 100;
+        const int BUTTON_SIZE_X = 100;//ボタンの横
+        const int BUTTON_SIZE_Y = 100;//ボタンの縦
 
-        const int BOARD_SIZE_X = 3;
-        const int BOARD_SIZE_Y = 3;
+        const int BOARD_SIZE_X = 3;//ボタンが横に何個並ぶか
+        const int BOARD_SIZE_Y = 3;//ボタンが縦に何個並ぶか
 
+        //testbuttonの二次元配列
         private testbutton[,] _buttonArray;
-
 
         public Form1()
         {
             InitializeComponent();
+
             //_buttonArrayの初期化
-            _buttonArray=new testbutton[BOARD_SIZE_Y, BOARD_SIZE_X];
+            _buttonArray = new testbutton[BOARD_SIZE_Y, BOARD_SIZE_X];
 
             for (int i = 0; i < BOARD_SIZE_X; i++)
             {
                 for (int j = 0; j < BOARD_SIZE_Y; j++)
                 {
                     //インスタンス生成
-                    testbutton Testbutton = new testbutton(new Point (BUTTON_SIZE_X * i, BUTTON_SIZE_Y * j),
-                                                           new Size(BUTTON_SIZE_X, BUTTON_SIZE_Y),"あ");
+                    testbutton Testbutton =
+                        new testbutton(
+                            this,
+                            i,j,
+                            new Size(BUTTON_SIZE_X, BUTTON_SIZE_Y),
+                              "");
 
                     //配列にボタンの参照を追加
                     _buttonArray[j, i] = Testbutton;
@@ -45,14 +51,24 @@ namespace Form_Test
                 }
             }
 
-            _buttonArray[1,2].SetEnable(true);
-            
+            _buttonArray[1, 0].SetEnable(true);
         }
+            public testbutton GetTestButton(int x, int y)
+        
+            
+            {
+                //配列外参照対策
+                if (x < 0 || x >= BOARD_SIZE_X) return null;
+                if (y < 0 || y >= BOARD_SIZE_Y) return null;
+           
+                return _buttonArray[y, x];
+            }
+      
         
         //自動生成
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("C#の世界へようこそ！");
+            MessageBox.Show("クリック");
         }
     }
 }
--- Form_Test/Form_Test/testbutton.cs
+++ Form_Test/Form_Test/testbutton.cs
@@ -8,48 +8,91 @@ using System.Windows.Forms;
 
 namespace Form_Test
 {
-    internal class testbutton : Button
+    public class testbutton : Button
     {
         //ボタンの色設定
-        private Color _onColor=Color.LightBlue;//ONの時の色
+        private Color _onColor = Color.LightBlue;//ONの時の色
+
         private Color _offColor = Color.DarkBlue;//OFFの時の色
 
         private bool _enable;
+
+        private Form1 _form1;
+
+        private int _x;
+
+        private int _y;
+
+
+
+        public testbutton(Form1 form1, int x, int y, Size size, string text)
+        {
+
+            _form1 = form1;
+
+            _x = x;
+
+            _y = y;
+            //ボタンの位置設定
+            Location = new Point(x * size.Width, y * size.Height);
+            //ボタンの大きさ設定
+            Size = size;
+            //ボタン内のテキスト設定
+            Text = text;
+
+            SetEnable(false);
+
+            Click += ClickEvent;
+        }
         public void SetEnable(bool on)
         {
-            _enable= on;
+            _enable = on;
             if (on)
             {
                 BackColor = _onColor;
             }
             else
             {
-                BackColor= _offColor;
+                BackColor = _offColor;
             }
-                
+
         }
 
-
-        public testbutton(Point position, Size size, string text)
-      {
-        //ボタンの位置設定
-        Location=position;
-        //ボタンの大きさ設定
-        Size=size;
-        //ボタン内のテキスト設定
-        Text=text;
-
-        SetEnable(false);
-
-        Click+=ClickEvent;
-       }
-
-        //自分で作成することも可能
-
-             private void ClickEvent(object sender, EventArgs e)
+        public void Toggle()
         {
-            SetEnable(!_enable);//クリックしたら切り替わるようになる
+            SetEnable(!_enable);
         }
+
+        private void ClickEvent(object sender, EventArgs e)
+        {
+            ////楽な書き方
+            //_form1.GetTestButton(_x, _y)?.Toggle();//?はnull安全
+            //_form1.GetTestButton(_x + 1, _y)?.Toggle();
+            //_form1.GetTestButton(_x - 1, _y)?.Toggle();
+            //_form1.GetTestButton(_x, _y + 1)?.Toggle();
+            //_form1.GetTestButton(_x, _y - 1)?.Toggle();
+
+            //かっこいい書き方
+            for (int i = 0; i < _toggleData.Length; i++)
+            {
+                var data = _toggleData[i];
+                var button = _form1.GetTestButton(_x + data[0], _y + data[1]);
+
+                if (button != null)
+                {
+                    button.Toggle();
+
+                }
+            }
+        }
+        private int[][] _toggleData =
+        {
+            new int[]{0,0},
+            new int[]{1, 0 },
+            new int[]{-1, 0},
+            new int[]{0, 1},
+            new int[]{0, -1},
+        };
     }
 }
 
